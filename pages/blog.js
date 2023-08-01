a1053219bdc60dda923723b757b913c88d7233b6
Joshua Blewitt <joshblewitt@protonmail.com>
2023-08-01 18:09:42 +0100

    New pages and ESLint added

---
+++ pages/blog.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+const postsPerPage = 5;
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  const allPostsNum = allPostsData.length;
+  const numPages = Math.ceil(allPostsData.length / postsPerPage);
+
+  return {
+    props: {
+      allPostsData: allPostsData.slice(0, postsPerPage),
+      numPages,
+      allPostsNum
+    },
+  };
+}
+
+export default function Home({ allPostsData, numPages, allPostsNum }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I'm Joshua Blewitt, hobbyist developer!</p> 
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog - {allPostsNum} posts</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+        {/* Pagination links */}
+        <div>
+          {Array.from({ length: numPages }, (_, i) => (
+            <Link style={{ margin: '0.5rem' }} href={`/page/${i + 1}`} key={i + 1}>
+              {i + 1}
+            </Link>
+            ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
--- pages/page/[pageNumber].js
+++ pages/page/[pageNumber].js
@@ -36,21 +36,16 @@ export async function getStaticProps({ params }) {
   };
 }
 
-export default function BlogPage({ currentPosts, numPages }) {
+export default function BlogPage({ currentPosts, numPages, pageNumber }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Joshua Blewitt, hobbyist developer!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Blog - Page {pageNumber}</h2>
         <ul className={utilStyles.list}>
           {currentPosts.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
