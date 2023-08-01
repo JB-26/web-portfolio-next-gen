73f800d5fe6b2ab8c7e2e78521f4751d2a6e7038
Joshua Blewitt <joshblewitt@protonmail.com>
2023-08-01 18:14:48 +0100

    ESLint fixes

--- pages/about.js
+++ pages/about.js
@@ -27,7 +27,7 @@ export default function Home({ allPostsData, numPages, allPostsNum }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm Joshua Blewitt, hobbyist developer!</p> 
+        <p>I&apos;m Joshua Blewitt, hobbyist developer!</p> 
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog - {allPostsNum} posts</h2>
