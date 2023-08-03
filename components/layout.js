30cc8d14c0d7725bff01e0274a9431da9cbc4660
Joshua Blewitt <joshblewitt@protonmail.com>
2023-08-03 16:24:15 +0100

    Signature added to blog posts

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,10 @@
-import Head from 'next/head';
-import styles from './layout.module.css';
-import Link from 'next/link';
-import Header from './header'
+import Head from "next/head";
+import styles from "./layout.module.css";
+import Link from "next/link";
+import Header from "./header";
+import Signature from "./signature";
 
-export const siteTitle = 'Joshua Blewitt';
+export const siteTitle = "Joshua Blewitt";
 
 export default function Layout({ children, home }) {
   return (
@@ -17,7 +18,7 @@ export default function Layout({ children, home }) {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle,
+            siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
@@ -26,9 +27,12 @@ export default function Layout({ children, home }) {
       <Header></Header>
       <main>{children}</main>
       {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/blog">← Back to blog</Link>
-        </div>
+        <>
+          <Signature></Signature>
+          <div className={styles.backToHome}>
+            <Link href="/blog">← Back to blog</Link>
+          </div>
+        </>
       )}
     </div>
   );
---
+++ components/signature.js
@@ -0,0 +1,32 @@
+import styles from "./signature.module.css";
+import Image from "next/image";
+import Photo from "../public/images/profile.webp";
+import Link from "next/link";
+
+export default function Signature() {
+  return (
+    <div className={styles.signature}>
+      <hr className={styles.line} />
+      <div className={styles.format}>
+        <div className={styles.image}>
+          <Image alt="A photo of me!" className={styles.image} src={Photo} />
+        </div>
+        <div>
+          <p className={styles.info}>
+            I&apos;m Joshua Blewitt, an analyst, hobbyist developer and writer.
+            I&apos;ve worked for companies such as Domino&apos;s and IQVIA.
+            Currently a Software Test Engineer.
+          </p>
+          <a href="mailto:joshblewitt@protonmail.com" className={styles.link}>
+            Let me know your thoughts!
+          </a>
+          <br />
+          <Link href="/about/" className={styles.link}>
+            More about me
+          </Link>
+        </div>
+      </div>
+      <hr className={styles.line} />
+    </div>
+  );
+}
