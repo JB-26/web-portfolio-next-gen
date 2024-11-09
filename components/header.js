0123deb8ab778447890c04f3e1ff145a907516fa
Joshua Blewitt <joshblewitt@protonmail.com>
2024-11-09 17:54:17 +0000

    New paragraph and reworked header

--- components/header.js
+++ components/header.js
@@ -5,37 +5,32 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header data-testid="header-component" className={styles.header}>
-      <div className={styles.headerItem}>
-        <Link href="/about/" className={styles.blog}>
-          About
-        </Link>
-      </div>
-      <div className={styles.headerItem}>
-        <Link href="/links/" className={styles.blog}>
-          Links
-        </Link>
-      </div>
-      <div className={styles.icon}>
-        <Link href="/" className={styles.blog}>
-          <Image
-            priority
-            src={Icon}
-            alt={"My initials - which is also the favicon"}
-            className={styles.logo}
-          />
-        </Link>
-      </div>
-      <div className={styles.headerItem}>
-        <Link href="/uses/" className={styles.blog}>
-          Uses
-        </Link>
-      </div>
-      <div className={styles.headerItem}>
-        <Link href="/blog/" className={styles.blog}>
-          Blog
-        </Link>
-      </div>
-    </header>
+    <>
+      <Link href="/" className={styles.blog}>
+        <h1 className={styles.headerTitle}>Joshua Blewitt</h1>
+      </Link>
+      <header data-testid="header-component" className={styles.header}>
+        <div className={styles.headerItem}>
+          <Link href="/about/" className={styles.blog}>
+            About
+          </Link>
+        </div>
+        <div className={styles.headerItem}>
+          <Link href="/links/" className={styles.blog}>
+            Links
+          </Link>
+        </div>
+        <div className={styles.headerItem}>
+          <Link href="/uses/" className={styles.blog}>
+            Uses
+          </Link>
+        </div>
+        <div className={styles.headerItem}>
+          <Link href="/blog/" className={styles.blog}>
+            Blog
+          </Link>
+        </div>
+      </header>
+    </>
   );
 }
