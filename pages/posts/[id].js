44e904cb3078b58da8d5a07e38e4430c51dc2d1b
Joshua Blewitt <joshblewitt@protonmail.com>
2025-07-30 18:03:30 +0100

    Drop cap CSS styling added

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,9 +3,10 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import postStyle from "../../styles/post.module.css";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
-import rehypeRaw from 'rehype-raw';
+import rehypeRaw from "rehype-raw";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -34,7 +35,10 @@ export default function Post({ postData }) {
         {/* Open Graph meta tags */}
         <meta property="og:title" content={postData.title} />
         <meta property="og:description" content={postData.description} />
-        <meta property="og:url" content={`https://www.joshblewitt.dev/posts/${postData.id}`} />
+        <meta
+          property="og:url"
+          content={`https://www.joshblewitt.dev/posts/${postData.id}`}
+        />
         <meta property="og:image:alt" content={postData.description} />
         <meta property="og:type" content="article" />
       </Head>
@@ -43,16 +47,20 @@ export default function Post({ postData }) {
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <ReactMarkdown rehypePlugins={[rehypeRaw]}>{postData.contentHtml}</ReactMarkdown>
+        <div className={postStyle.dropCap}>
+          <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+            {postData.contentHtml}
+          </ReactMarkdown>
+        </div>
       </article>
       <div className={utilStyles.tags}>
-        <h1 className={utilStyles.headingLg}>Tags:{" "}</h1>
+        <h1 className={utilStyles.headingLg}>Tags: </h1>
         {postData.tags.map((tag) => (
           <Link key={tag} href={`/tags/${tag}`} className={utilStyles.tag}>
             {tag}
           </Link>
-          ))}
+        ))}
       </div>
     </Layout>
-    );
+  );
 }
